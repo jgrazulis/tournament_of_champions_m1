4986dede2938670bf44d8670f02e19d1a2125eeb
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-18 10:00:25 -0700

    Add test for competition names link

---
+++ spec/features/competitions/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe 'competition index' do
+  before(:each) do
+    @denver = Competition.create!(name: "Mile High Soccer Championship", location: "Denver, CO", sport: "soccer")
+    @chicago = Competition.create!(name: "Midwest Soccer Championship", location: "Chicago, IL", sport: "soccer")
+    @seattle = Competition.create!(name: "Pacific Northwest Soccer Championship", location: "Seattle, WA", sport: "soccer")
+  end
+
+  scenario 'visitor sees names of all competitions as links' do
+    visit "/competitions"
+    expect(page).to have_link "#{@denver.name}"
+    expect(page).to have_link "#{@chicago.name}"
+    expect(page).to have_link "#{@seattle.name}"
+  end
+end
