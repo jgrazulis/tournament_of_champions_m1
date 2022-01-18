26a3ebef307c4ebc9128dfb9164d754f34d4fe05
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-18 10:46:24 -0700

    Add additional info to controller show method

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -5,5 +5,6 @@ class CompetitionsController < ApplicationController
 
   def show 
     @competition = Competition.find(params[:id])
+    @team_competition_players = @competition.teams.joins(:players)
   end
 end
