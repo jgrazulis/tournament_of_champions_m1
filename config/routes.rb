67cb14c4effb9b87f35cd03fb21f7813b63ec0e0
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-18 11:40:48 -0700

    Add routes for teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :competitions, only: [:index, :show, :create, :new]
+  resources :competitions, only: [:index, :show] do
+    resources :teams, only: [:new, :create] do
+    end
+  end 
 end
