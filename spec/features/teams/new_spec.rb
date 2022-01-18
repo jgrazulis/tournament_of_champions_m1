39f5837a9184f6d9aeb417244c4fb3f43aeb0a20
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-18 11:40:13 -0700

    Add test for form link

---
+++ spec/features/teams/new_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'new team form' do
+  before(:each) do
+    @denver = Competition.create!(name: "Mile High Soccer Championship", location: "Denver, CO", sport: "soccer")
+    @rockies = @denver.teams.create!(nickname: "Little Rockies", hometown: "Denver, CO")
+    @soccer5280 = @denver.teams.create!(nickname: "Soccer 5280", hometown: "Denver, CO")
+    @rapids = @denver.teams.create!(nickname: "Colorado Rapids", hometown: "Denver, CO")
+    @jamie = @rapids.players.create!(name: "Jamie Tartt", age: 20)
+    @roy = @rapids.players.create!(name: "Roy Kent", age: 30)
+    @sam = @rapids.players.create!(name: "Sam Obisanya", age: 20)
+    @ted = @rapids.players.create!(name: "Ted Lasso", age: 20)
+    @seattle = Competition.create!(name: "Pacific Northwest Soccer Championship", location: "Seattle, WA", sport: "soccer")
+    @mariners = @seattle.teams.create!(nickname: "Seattle Mariners", hometown: "Seattle, WA")
+    @bob = @mariners.players.create!(name: "Bob Vance", age: 25)
+  end
+
+  scenario 'user clicks link and is taken to a form to register new team' do
+    visit "/competitions/#{@denver.id}"
+    click_link "Register a new team"
+    expect(current_path).to eq("/competitions/#{@denver.id}/teams/new")
+    redirect_to "/competitions/#{@denver.id}"
+  end
+end 
