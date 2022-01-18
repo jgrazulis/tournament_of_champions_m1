1c0d3b6b1fd7d3ad391a0878001b4b18543d9dd6
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-18 12:00:45 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :competitions, only: [:index, :show] do
-    resources :teams, only: [:new, :create] do
+    resources :teams, only: [:index, :new, :create] do
     end
   end 
 end
